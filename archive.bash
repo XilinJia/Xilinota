0c69cb795953517f69b772b058d4e59f8da1c7ea
Xilin Jia <6257601+XilinJia@users.noreply.github.com>
2023-12-20 21:00:38 +0100

    minor git cleanup

--- archive.bash
+++
@@ -1,4 +0,0 @@
-
-#! /bin/bash
-
-rsync -vurt --exclude '*.map' --exclude=**/.gradle/ --exclude=**/vendor/ --exclude=**/*node_module*/ --exclude=**/*cache*/ --exclude=**/dist/ --exclude=**/tmp/ --exclude=**/generated/ --exclude=**/intermediates/ --exclude=**/outputs/ . ../.OldFiles/Xilinota
